cbc9dc75e80c26c1aa05f060665925c3484149c2
FSVD <fabio.schettino.01@gmail.com>
2019-08-28 20:47:08 +0200

    updated

--- resources/views/layout/partials/nav.blade.php
+++ resources/views/layout/partials/nav.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="/">Review Analyzer</a>
+  <a class="navbar-brand" href="{{ url('/') }}">Review Analyzer</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -14,8 +14,8 @@
           Keywords
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="/whitelist">Whitelist</a>
-          <a class="dropdown-item" href="/blacklist">Blacklist</a>
+          <a class="dropdown-item" href="{{ url('/whitelist') }}">Whitelist</a>
+          <a class="dropdown-item" href="{{ url('/blacklist') }}">Blacklist</a>
         </div>
       </li>
     </ul>
