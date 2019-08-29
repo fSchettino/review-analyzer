93b82493f9555c30bf1b03ee9d582a5c726233b7
Fabio Schettino <fabio.schettino.01@gmail.com>
2019-08-29 16:34:49 +0200

    relations added

--- app/Http/Models/WhitelistKeyword.php
+++ app/Http/Models/WhitelistKeyword.php
@@ -14,4 +14,16 @@ class WhitelistKeyword extends Model
         'name',
         'weight',
     ];
+
+    //each keyword belongs to many services
+    public function services()
+    {
+        return $this->belongsToMany('App\Http\Models\Service');
+    }
+
+    //each keyword belongs to many hotels
+    public function hotels()
+    {
+        return $this->belongsToMany('App\Http\Models\Hotel');
+    }
 }
