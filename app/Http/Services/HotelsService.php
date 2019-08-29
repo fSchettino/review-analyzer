70459e46092f7025a31141e234d31652631037e3
Fabio Schettino <fabio.schettino.01@gmail.com>
2019-08-29 12:52:31 +0200

    updated

--- app/Http/Services/HotelsService.php
+++ app/Http/Services/HotelsService.php
@@ -9,9 +9,9 @@ class HotelsService
     public function getHotelsList()
     {
         $hotels = [
-            0 => ['id' => 1, 'name' => 'Hotel 1', 'avgScore' => '5.5',],
-            1 => ['id' => 2, 'name' => 'Hotel 2', 'avgScore' => '4.5',],
-            2 => ['id' => 3, 'name' => 'Hotel 3', 'avgScore' => '3.5',]
+            0 => ['id' => 1, 'name' => 'Hotel 1', 'avgScore' => '5.5', 'goodKeywords' => ['good', 'exelent', 'awesome'], 'badKeywords' => ['bad', 'dreadful', 'appalling']],
+            1 => ['id' => 2, 'name' => 'Hotel 2', 'avgScore' => '4.5', 'goodKeywords' => ['good', 'exelent', 'awesome'], 'badKeywords' => ['bad', 'dreadful', 'appalling']],
+            2 => ['id' => 3, 'name' => 'Hotel 3', 'avgScore' => '3.5', 'goodKeywords' => ['good', 'exelent', 'awesome'], 'badKeywords' => ['bad', 'dreadful', 'appalling']]
         ];
 
         return $hotels;
