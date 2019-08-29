83fd48d6edd422c6a05cd105e3e6429a23b1f603
Fabio Schettino <fabio.schettino.01@gmail.com>
2019-08-29 16:36:24 +0200

    add form added

--- resources/views/keywords/whitelist/add.blade.php
+++ resources/views/keywords/whitelist/add.blade.php
@@ -10,5 +10,27 @@
             <a class="btn btn-primary float-right" href="{{ url('/whitelist') }}" role="button">Back</a>
         </div>
     </div>
+    <!-- Start add form -->
+    <div class="row margin-top-20">
+        <div class="col-md-8 col-sm-12">
+            <form method="post" action="{{ url('/whitelistKey') }}">
+            @csrf
+                <div class="form-group">
+                    <label for="name">Keyword name</label>
+                    <input type="text" class="form-control" placeholder="e.g. good" id="name" name="name">
+                </div>
+                <div class="form-group">
+                    <label for="weight">Keyword weight</label>
+                    <select class="form-control" id="weight" name="weight">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-primary">Save</button>
+            </form>
+        </div>
+    </div>
+    <!-- End add form -->
 </div>
 @endsection
