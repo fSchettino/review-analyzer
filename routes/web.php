fa4bc6409a1de764d575de9dabfaaa83f7636c3f
Fabio Schettino <fabio.schettino.01@gmail.com>
2019-09-06 15:45:02 +0200

    routes updated

--- routes/web.php
+++ routes/web.php
@@ -15,36 +15,33 @@
 Route::View('/', 'index');
 
 // Hotels routes
-Route::get('hotels', 'HotelsController@index');
-Route::get('hotel/{id}', 'HotelsController@show');
-Route::match(['get', 'post'], 'hotel', 'HotelsController@add');
+Route::get('hotels', 'HotelsController@all');
+Route::get('hotel/{id}', 'HotelsController@find');
+Route::match(['get', 'post'], 'hotel', 'HotelsController@create');
 Route::match(['get', 'post'], 'hotel/edit/{id}', 'HotelsController@edit');
 Route::get('hotel/delete/{id}', 'HotelsController@delete');
 
 // Services routes
-Route::get('services', 'ServicesController@index');
-Route::get('service/{id}', 'ServicesController@show');
-Route::match(['get', 'post'], 'service', 'ServicesController@add');
+Route::get('services', 'ServicesController@all');
+Route::get('service/{id}', 'ServicesController@find');
+Route::match(['get', 'post'], 'service', 'ServicesController@create');
 Route::match(['get', 'post'], 'service/edit/{id}', 'ServicesController@edit');
 Route::get('service/delete/{id}', 'ServicesController@delete');
 
 // Reviews routes
-Route::get('reviews/{hotel_id}', 'ReviewsController@index');
-Route::get('review/{id}', 'ReviewsController@show');
-Route::match(['get', 'post'], 'review/hotel/{hotel_id}', 'ReviewsController@add');
-Route::match(['get', 'post'], 'review/edit/{id}', 'ReviewsController@edit');
+Route::match(['get', 'post'], 'review/hotel/{hotel_id}', 'ReviewsController@create');
 Route::get('review/delete/{id}', 'ReviewsController@delete');
 
 // Keywords routes
-Route::get('keywords', 'KeywordsController@index');
-Route::get('keyword/{id}', 'KeywordsController@show');
-Route::match(['get', 'post'], 'keyword', 'KeywordsController@add');
+Route::get('keywords', 'KeywordsController@all');
+Route::get('keyword/{id}', 'KeywordsController@find');
+Route::match(['get', 'post'], 'keyword', 'KeywordsController@create');
 Route::match(['get', 'post'], 'keyword/edit/{id}', 'KeywordsController@edit');
 Route::get('keyword/delete/{id}', 'KeywordsController@delete');
 
 // Rueles routes
-Route::get('rules', 'RulesController@index');
-Route::get('rule/{id}', 'RulesController@show');
-Route::match(['get', 'post'], 'rule', 'RulesController@add');
+Route::get('rules', 'RulesController@all');
+Route::get('rule/{id}', 'RulesController@find');
+Route::match(['get', 'post'], 'rule', 'RulesController@create');
 Route::match(['get', 'post'], 'rule/edit/{id}', 'RulesController@edit');
 Route::get('rule/delete/{id}', 'RulesController@delete');
