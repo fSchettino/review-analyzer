66aa7e82152bc0f1534ad6f05d559189012f5e9c
Fabio Schettino <fabio.schettino.01@gmail.com>
2019-09-06 15:40:06 +0200

    interfaces implementations bindings added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,60 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Hotel bindings
+        $this->app->bind(
+            'App\Http\Interfaces\HotelsServiceInterface',
+            'App\Http\Services\HotelsService'
+        );
+        
+        $this->app->bind(
+            'App\Http\Interfaces\HotelRepositoryInterface',
+            'App\Http\Repositories\HotelRepository'
+        );
+        
+        // Keyword bindings
+        $this->app->bind(
+            'App\Http\Interfaces\KeywordsServiceInterface',
+            'App\Http\Services\KeywordsService'
+        );
+        
+        $this->app->bind(
+            'App\Http\Interfaces\KeywordRepositoryInterface',
+            'App\Http\Repositories\KeywordRepository'
+        );
+
+        // Review bindings
+        $this->app->bind(
+            'App\Http\Interfaces\ReviewsServiceInterface',
+            'App\Http\Services\ReviewsService'
+        );
+
+        $this->app->bind(
+            'App\Http\Interfaces\ReviewRepositoryInterface',
+            'App\Http\Repositories\ReviewRepository'
+        );
+
+        // Rule bindings
+        $this->app->bind(
+            'App\Http\Interfaces\RulesServiceInterface',
+            'App\Http\Services\RulesService'
+        );
+
+        $this->app->bind(
+            'App\Http\Interfaces\RuleRepositoryInterface',
+            'App\Http\Repositories\RuleRepository'
+        );
+
+        // Service bindings
+        $this->app->bind(
+            'App\Http\Interfaces\ServicesServiceInterface',
+            'App\Http\Services\ServicesService'
+        );
+
+        $this->app->bind(
+            'App\Http\Interfaces\ServiceRepositoryInterface',
+            'App\Http\Repositories\ServiceRepository'
+        );
     }
 
     /**
