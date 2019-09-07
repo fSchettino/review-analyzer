9d00c1487d5ecece0f0a3923cebe604162d1807f
FSVD <fabio.schettino.01@gmail.com>
2019-09-07 22:31:39 +0200

    reverted to initial state

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,60 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // Hotel bindings
-        $this->app->bind(
-            'App\Modules\Hotel\Interfaces\HotelServiceInterface',
-            'App\Modules\Hotel\HotelService'
-        );
-        
-        $this->app->bind(
-            'App\Modules\Hotel\Interfaces\HotelRepositoryInterface',
-            'App\Modules\Hotel\HotelRepository'
-        );
-
-        // Keyword bindings
-        $this->app->bind(
-            'App\Modules\Keyword\Interfaces\KeywordServiceInterface',
-            'App\Modules\Keyword\KeywordService'
-        );
-        
-        $this->app->bind(
-            'App\Modules\Keyword\Interfaces\KeywordRepositoryInterface',
-            'App\Modules\Keyword\KeywordRepository'
-        );
-
-        // Review bindings
-        $this->app->bind(
-            'App\Modules\Review\Interfaces\ReviewServiceInterface',
-            'App\Modules\Review\ReviewService'
-        );
-
-        $this->app->bind(
-            'App\Modules\Review\Interfaces\ReviewRepositoryInterface',
-            'App\Modules\Review\ReviewRepository'
-        );
-
-        // Rule bindings
-        $this->app->bind(
-            'App\Modules\Rule\Interfaces\RuleServiceInterface',
-            'App\Modules\Rule\RuleService'
-        );
-
-        $this->app->bind(
-            'App\Modules\Rule\Interfaces\RuleRepositoryInterface',
-            'App\Modules\Rule\RuleRepository'
-        );
-
-        // Service bindings
-        $this->app->bind(
-            'App\Modules\Service\Interfaces\ServiceServiceInterface',
-            'App\Modules\Service\ServiceService'
-        );
-
-        $this->app->bind(
-            'App\Modules\Service\Interfaces\ServiceRepositoryInterface',
-            'App\Modules\Service\ServiceRepository'
-        );
+        //
     }
 
     /**
@@ -77,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
+        //
     }
 }
