37576407beae754337fd5fb3fee9567485d4e0fb
FSVD <fabio.schettino.01@gmail.com>
2019-09-07 23:34:47 +0200

    modules route mapping added

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -39,21 +39,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
-        //
-    }
-
-    /**
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @return void
-     */
-    protected function mapWebRoutes()
-    {
-        Route::middleware('web')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/web.php'));
+        $this->mapModulesRoutes();
     }
 
     /**
@@ -69,5 +55,44 @@ class RouteServiceProvider extends ServiceProvider
              ->middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
+    }
+    
+    /**
+     * Define the "web" routes for the application.
+     *
+     * These routes all receive session state, CSRF protection, etc.
+     *
+     * @return void
+     */
+    protected function mapWebRoutes()
+    {
+        Route::middleware('web')
+             ->namespace($this->namespace)
+             ->group(base_path('routes/web.php'));
+    }
+    
+    /**
+     * Define the routes for the application modules.
+     *
+     * These routes all receive session state, CSRF protection, etc.
+     *
+     * @return void
+     */
+    protected function mapModulesRoutes()
+    {
+        Route::namespace('App\Modules\Hotel')
+             ->group(base_path('App\Modules\Hotel\HotelRouter.php'));
+
+        Route::namespace('App\Modules\Keyword')
+             ->group(base_path('App\Modules\Keyword\KeywordRouter.php'));
+
+        Route::namespace('App\Modules\Review')
+             ->group(base_path('App\Modules\Review\ReviewRouter.php'));
+
+        Route::namespace('App\Modules\Rule')
+             ->group(base_path('App\Modules\Rule\RuleRouter.php'));
+
+        Route::namespace('App\Modules\Service')
+             ->group(base_path('App\Modules\Service\ServiceRouter.php'));
     }
 }
