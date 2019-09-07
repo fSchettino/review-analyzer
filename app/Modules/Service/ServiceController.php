4e20f86f593570f2ff5af3ae9c69a71ae2159f08
FSVD <fabio.schettino.01@gmail.com>
2019-09-07 23:33:18 +0200

    inherited class path updated

--- app/Modules/Service/ServiceController.php
+++ app/Modules/Service/ServiceController.php
@@ -4,6 +4,7 @@ namespace App\Modules\Service;
 
 use Illuminate\Http\Request;
 
+use App\Modules\Shared\Controllers\Controller;
 use App\Modules\Service\Interfaces\ServiceServiceInterface;
 
 class ServiceController extends Controller
