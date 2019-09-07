ab9abb3c5ce6406997a52a3fadcc49639c657e70
FSVD <fabio.schettino.01@gmail.com>
2019-09-07 23:47:39 +0200

    updated

--- database/migrations/2019_08_29_111123_create_database.php
+++ database/migrations/2019_08_29_111123_create_database.php
@@ -13,26 +13,6 @@ class CreateDatabase extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-
-            $table->engine = 'MyISAM';
-        });
-
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-
-            $table->engine = 'MyISAM';
-        });
-
         Schema::create('hotel', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name', 100);
